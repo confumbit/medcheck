31ad96b85b26e7de245cc18907d6a7428ea16392
Heatblast <heatblast0044@gmail.com>
2021-09-20 16:58:14 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./app/screens/Home";
+import List from "./app/screens/List";
+import Edit from "./app/screens/Edit";
+import Add from "./app/screens/Add";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  console.log("App rendered.");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="List" component={List} />
+          <Stack.Screen
+            name="Edit"
+            component={Edit}
+            options={{ title: "Edit Item" }}
+          />
+          <Stack.Screen
+            name="Add"
+            component={Add}
+            options={{ title: "Add Item" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
